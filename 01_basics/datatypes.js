ce180d28408003e0047b6a9db52c031949cd5f67
ajitkum1 <39055607+ajitkum1@users.noreply.github.com>
2025-02-26 10:28:59 +0000

    datatypecheck

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+// number => 2 to power of 53
+// bigint  -- jsed in facebook/redit
+// string = "RS"
+// boolean = true/false
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => for unique ness
+
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,19 @@
+"use strict" //treat all JS Code as newer version
+// alert (3+3) // we are using nodejs, not browser
+
 console.log("RS")
+
+console.log("akc")
+// prepfer not to use var datatypes because of issue in block scope and functional scope.
+
+const accountId =12345
+let accountEmail="abc@abc.com" //Cannot redeclare block-scoped variable
+var accountPassword="abc432"
+accountCity="Mumbai"
+let accountState;
+ accountEmail="GGG@abc.com" // you can assign a new value 
+
+//accountId=12
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
