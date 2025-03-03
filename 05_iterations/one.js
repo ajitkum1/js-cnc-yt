95e97271450a142d6322557c2c6ff071f5426cde
ajitkum1 <39055607+ajitkum1@users.noreply.github.com>
2025-03-03 11:20:09 +0000

    forof

---
+++ 05_iterations/one.js
@@ -0,0 +1,10 @@
+const arr = [1,2,3,4,5,6,7]
+for (const num of arr) {
+    console.log(num)
+}
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    console.log(` Each char of greet is ${greet}`)
+}
+
